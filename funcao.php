f2c4a649f06c4c4338b7d817aa910f07b19c5eff
ThalesHenr <thaleshenr46@gmail.com>
2022-05-13 21:32:18 -0300

    arquivo função.php criada

---
+++ funcao.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<br>";
+
+function Msg(){
+
+    echo "Estopu dentro de uma função"/
+}
+
+echo "Este texto está fora da função, ou seja script geral";
+
+//chamar uma função 
+echo "<br>";
+Msg()/
+?>
